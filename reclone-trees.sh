1e9d15d433a6b93a9cc0446181fa3a996cfdaf87
Majaahh <maja.blaszczak@proton.me>
2025-09-08 22:19:01 +0200

    s5e8825: Bump to lineage-23.0
    Change-Id: I04d355f1f4291136f44640c744b17204f8391ecb
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- reclone-trees.sh
+++ reclone-trees.sh
@@ -8,7 +8,7 @@ DEVICES=(
 XML="$(pwd)/.repo/local_manifests/s5e8825.xml"
 URL="https://github.com/exynos1280"
 [ "$(whoami)" = "ksawlii" ] && URL="git@github.com-ksawlii:exynos1280"
-[ -z "$BRANCH" ] && BRANCH="lineage-22.2"
+[ -z "$BRANCH" ] && BRANCH="lineage-23.0"
 
 if [ ! -f "$XML" ]; then
   echo "s5e8825.xml not found. Please execute from root dir."
@@ -18,7 +18,7 @@ fi
 for m in "${DEVICES[@]}"; do
   sed -i "/$m/d" "$XML"
   [ -d "$(pwd)/device/samsung/$m" ] && rm -rf "$(pwd)/device/samsung/$m"
-  git clone -j"$(nproc --all)" "$URL/android_device_samsung_$m" "$(pwd)/device/samsung/$m"
+  git clone -j"$(nproc --all)" "$URL/android_device_samsung_$m" -b "$BRANCH" "$(pwd)/device/samsung/$m"
   [ -d "$(pwd)/vendor/samsung/$m" ] && rm -rf "$(pwd)/vendor/samsung/$m"
-  git clone -j"$(nproc --all)" "$URL/proprietary_vendor_samsung_$m" "$(pwd)/vendor/samsung/$m"
+  git clone -j"$(nproc --all)" "$URL/proprietary_vendor_samsung_$m" -b "$BRANCH" "$(pwd)/vendor/samsung/$m"
 done
